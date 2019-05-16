f15a7cad7307d0bc863886ce48df05185f97b3be
Agroier <email@example.com>
2019-05-16 21:45:06 +0800

    enum first test

---
+++ enumtest/enumtest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumtest
+{
+        enum orientation :byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            orientation myDirection = orientation.north;
+            Console.WriteLine($"myDirection = {myDirection}");
+            Console.ReadKey();
+        }
+    }
+}
