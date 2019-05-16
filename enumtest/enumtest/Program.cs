dcfba73d7413a5fd6275c3c15925aef45d9f2155
Agroier <email@example.com>
2019-05-16 21:49:12 +0800

    enum scond test

--- enumtest/enumtest/Program.cs
+++ enumtest/enumtest/Program.cs
@@ -18,8 +18,14 @@ namespace enumtest
     {
         static void Main(string[] args)
         {
+            byte directionByte;
+            string directionString;
             orientation myDirection = orientation.north;
             Console.WriteLine($"myDirection = {myDirection}");
+            directionByte = (byte)myDirection;
+            directionString = Convert.ToString(myDirection);
+            Console.WriteLine($"byte equivalent = {directionByte}");
+            Console.WriteLine($"string equvalent = {directionString}");
             Console.ReadKey();
         }
     }
