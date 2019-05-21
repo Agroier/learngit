f3ab3d3542d7fd83ef505373787df210b9a84df0
Agroier <email@example.com>
2019-05-21 21:45:02 +0800

    Foreachtest

---
+++ foreachtest/foreachtest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace foreachtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Todd Athony ", "Kevin Holton", "Shane Laigle" };
+            WriteLine($"Here are {friendNames.Length} of my friend");
+            foreach (string friendName in friendNames)
+            {
+                WriteLine(friendName);
+            }
+            ReadKey();
+        }
+    }
+}
